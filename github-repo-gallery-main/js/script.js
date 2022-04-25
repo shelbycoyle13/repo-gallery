901d3de146dbc5d7a33077e813fbf97562330edd
Shelby Coyle <81052674+shelbycoyle13@users.noreply.github.com>
2022-04-24 23:50:41 -0400

    Fetched repo data and made visible on the page

--- github-repo-gallery-main/js/script.js
+++ github-repo-gallery-main/js/script.js
@@ -1,6 +1,7 @@
 // Variable to select the overview div //
-const overviewDiv = document.querySelector(".overview")
+const overviewDiv = document.querySelector(".overview");
 const username = "shelbycoyle13";
+const selectRepoList = document.querySelector(".repo-list");
 const getProfileInfo = async function() {
     const res = await fetch(`https://api.github.com/users/${username}`);
     const data = await res.json();
@@ -23,4 +24,20 @@ const displayProfileInfo = function(data) {
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
       </div>`;
     overviewDiv.append(createUserInfoDiv);
+    getRepos();
+};
+
+const getRepos = async function() {
+    const resRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const dataRepos = await resRepos.json();
+    displayRepos(dataRepos);
+};
+
+const displayRepos = function(repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        selectRepoList.append(repoItem);
+    }
 };
